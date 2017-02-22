9c470986b7f23590bc26f6e74f901b8bb4b9b4d2
NadimRai <rai_nadim@yahoo.com>
2017-02-22 10:15:31 -0800

    created components subfolder and user component ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>
+  	Hello {{name}}
+  	</h1>
+  	<h3> Your email add {{ email }}
+  	</h3>
+  	<p> Address: {{address.street}} {{ address.city}}, {{ address.state}} </p>
+  	`
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+	name = 'John Doe';
+	email = "johnDoe@example.com";
+	address = {
+		street: '12 Main Street',
+		city: 'Boston',
+		state: 'MA'
+	}
+ }
