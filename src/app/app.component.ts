2cde3d2d300cc6d9c1afc699d9b722ffc2545e48
NadimRai <rai_nadim@yahoo.com>
2017-02-22 11:05:04 -0800

    use if for and constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>
-  	Hello {{name}}
-  	</h1>
-  	<h3> Your email add {{ email }}
-  	</h3>
-  	<p> Address: {{address.street}} {{ address.city}}, {{ address.state}} </p>
-  	`
+  template: `
+   <user></user>
+  	`,
 })
 export class AppComponent  { 
-	name = 'John Doe';
-	email = "johnDoe@example.com";
-	address = {
-		street: '12 Main Street',
-		city: 'Boston',
-		state: 'MA'
-	}
+	
  }
